078a62db4c9d488216a1b8e59f33cdf651cdcb11
João Marcus <joaomarcus75@gmail.com>
2023-05-05 12:10:21 -0300

    Contact Model and other things
    Contact Model creation and fields validations with dataAnnotation,  get the data through the Form using the [FromForm] and validate if the the Model state is valid

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WordBank.Models;
 
 namespace WordBank.Controllers
 {
@@ -12,5 +13,37 @@ namespace WordBank.Controllers
         {
             return View();
         }
+
+        public IActionResult ReceiveContact([FromForm]Contact contact)
+        {
+            if(ModelState.IsValid)
+            {
+                string content = string.Format("Name: {0}, E-mail: {1}, Subject: {2}, Message: {3}", contact.Name, contact.Email, contact.Subject, contact.Message);
+                return new ContentResult() { Content = content };
+            }
+            else
+            {
+                return View("Index");
+            }
+            
+        }
+
+        //Get Data manually
+
+        //public IActionResult ReceiveContact()
+        //{
+        //    //Post - Request.Form
+        //    //GET - Request.QueryString
+        //    Contact contact = new Contact();   
+
+        //    contact.Name = Request.Form["name"];
+        //    contact.Email = Request.Form["email"];
+        //    contact.Subject = Request.Form["subject"];
+        //    contact.Message = Request.Form["message"];
+
+        //    string content = string.Format("Name: {0}, E-mail: {1}, Subject: {2}, Message: {3}",contact.Name,contact.Email,contact.Subject,contact.Message);
+
+        //    return new ContentResult(){ Content = content };
+        //}
     }
 }
---
+++ Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WordBank.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "this field is required")]
+        [MaxLength(50, ErrorMessage = "this field only supports 50 caracters")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "this field is required")]
+        [MaxLength(70,ErrorMessage = "this field only supports 70 caracters")]
+        [EmailAddress(ErrorMessage = "this field is invalid")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "this field is required")]
+        [MaxLength(70,ErrorMessage = "this field only supports 70 caracters")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "this field is required")]
+        [MaxLength(2000, ErrorMessage = "this field only supports 2000 caracters")]
+        public string Message { get; set; }
+    }
+}
